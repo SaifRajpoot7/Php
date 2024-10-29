461eb77249d5ae8ea305de78551f8a517537d348
SaifRajpoot7 <saif.itdeveloper@gmail.com>
2024-10-29 16:59:49 +0500

    Login System branch adding

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ $database = "mysql:host=localhost;dbname=notes_taking";
 $conn = new PDO($database,$username,$password);
 
 // $conn = mysqli_connect($servername, $username, $password, $database);
-
+$file_path = "http://localhost/php/crud_app_PDO";
 require_once("functions.php");
 
 ?>
---
+++ views/login_view.php
@@ -0,0 +1,14 @@
+<?php
+include('header.php');
+?>
+<form>
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
